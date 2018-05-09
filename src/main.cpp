7a3b810568f2e36061cf509a42435b7c98a2d349
Your Name <you@example.com>
2018-05-09 18:11:50 +0700

    Update read_command() trong main.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,26 @@
 
 int main() {
     try {
-        auto listener = net::TcpListener::bind(10000);
-        auto stream1 = net::TcpStream::connect("127.0.0.1", 10000);
-        auto stream2 = listener.next();
+        auto stream1 = net::TcpStream::connect("127.0.0.1", 21);
+
+        char passwd[] = "PASS 366117187\r\n";
+        char name[] = "USER thiennu\r\n";
+
+        char buf[200];
+        int len;
+
+        stream1.read(buf, 200);
+        std::cout << buf << std::endl;
+
+        stream1.write(name, sizeof(name)-1);
+        len = stream1.read(buf, 200);
+        buf[len] = 0;
+        std::cout << buf << std::endl;
+
+        stream1.write(passwd, sizeof(passwd)-1);
+        len = stream1.read(buf, 200);
+        buf[len] = 0;
+        std::cout << buf << std::endl;
     } catch (const char *e) {
         std::cout << e << std::endl;
     }
